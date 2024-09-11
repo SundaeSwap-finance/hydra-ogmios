40b3e5c02099023437a1a57b8cdf9bb60abbda27
rrruko <rukokarasu@gmail.com>
2024-09-11 16:26:42 -0700

    implement deserialize for StateChanged event TickObserved

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+use serde::{Deserialize, Serialize};
+
+const TICK_OBSERVED_1: &str = "a2676576656e744964191eaf6c73746174654368616e676564a269636861696e536c6f741941ee637461676c5469636b4f62736572766564";
+
+// src/Hydra/Events.hs 'StateEvent'
+// This is the type sent to EventSinks.
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Event {
+    event_id: u64,
+    state_changed: StateChanged,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct StateChanged {
+    chain_slot: u64,
+    tag: String,
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use ciborium::de::{from_reader, Error};
+    use hex;
+
+    #[test]
+    fn tick_observed_can_parse() {
+        let tick_observed_1_raw = hex::decode(TICK_OBSERVED_1).unwrap();
+        let result: Result<Event, Error<std::io::Error>> = from_reader(&tick_observed_1_raw[..]);
+        match result {
+            Ok(r) => {
+                println!("Event: {:?}", r)
+            }
+            Err(e) => {
+                panic!("{}", e)
+            }
+        }
+    }
 }
