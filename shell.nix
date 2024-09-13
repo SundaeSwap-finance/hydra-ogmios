32968238d622c89b0623669a1f52cc0147ebe7a4
rrruko <rukokarasu@gmail.com>
2024-09-13 02:19:17 -0700

    shell for nightly because naersk is teh suxxorz

--- shell.nix
+++ shell.nix
@@ -1,7 +1,13 @@
-(import (
-  fetchTarball {
-    url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
-    sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2"; }
-) {
-  src =  ./.;
-}).shellNix
+{ rustDate ? "2024-09-12" }:
+
+let
+  mozillaOverlay = import (builtins.fetchTarball "https://github.com/mozilla/nixpkgs-mozilla/archive/9b11a87c0cc54e308fa83aac5b4ee1816d5418a2.tar.gz");
+  pkgs = import <nixpkgs> {
+    overlays = [ mozillaOverlay ];
+  };
+  rustChannel = pkgs.rustChannelOf { date = rustDate; channel = "nightly"; };
+in pkgs.mkShell {
+  nativeBuildInputs = [
+    rustChannel.rust
+  ];
+}
